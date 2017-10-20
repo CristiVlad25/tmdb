f9c05e67563ff650e27791481accf89e498490cd
Cristi <CristiVlad25@users.noreply.github.com>
2017-10-20 18:25:54 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
 var loadResponse = function (data) {
+    $('#response1').html('');
+    $('#pageno').html('');
     $('#response1').append('<center><div style="color:#73AD21"><h2>Results:</h2></div></center>');
-    $('#pageno').append('<center>Page '+data.page+'</center>');
+    $('#pageno').append('<center>Page '+data.page+'<br><br></center>');
     
     for (var i=0; i<data.results.length; i++) {
-        
-        $('#response1').append('<br>'+(i+1)+'.'+'<br><br><img src=http://image.tmdb.org/t/p/w75/'+data.results[i].poster_path+'><br>');
-        $('#response1').append('<br>Popularity: '+data.results[i].popularity+'<br>');
-        $('#response1').append('Title: '+data.results[i].title+'<br>');
-        $('#response1').append('Vote Count: '+data.results[i].vote_count+'<hr>');};
-    };
+
+        $('#response1').append('<div class="response2"><br><div id="titl">'+(i+1)+'. '+data.results[i].title+'</div>'+
+        '<br><img src=http://image.tmdb.org/t/p/w75/'+data.results[i].poster_path+'><br><br>'+
+        '<div id="descr">Description: '+data.results[i].overview+'</div><br>'+
+        '<div style="align:right">Popularity/Vote Count: '+data.results[i].popularity+'/'+
+        data.results[i].vote_count+'<br><br></div></div><br>');};};
+
+document.getElementById('refr').onclick = function() {
+    $('#response1').html('');
+    $('#pageno').html('');
+}
     
 document.getElementById('myclick').onclick = function() {
-    var year = $('#txt1').val();
-    var votec = $('#txt2').val();
-    var language = $('#txt3').val();
-    var url = 'https://api.themoviedb.org/3/discover/movie?api_key=869477feed128ffc52a2405dfb5948e5&language='+language+'&vote_count.lte='+votec+'&year='+year;
     
-    $.ajax({url:url, method:'GET', async:true}).done(loadResponse);
+    var keyword = $('#txt1').val();
+    var year = $('#txt2').val();
+
+    var url_first = 'https://api.themoviedb.org/3/search/movie?api_key=869477feed128ffc52a2405dfb5948e5&query='+keyword;
+    var url_second = 'https://api.themoviedb.org/3/discover/movie?api_key=869477feed128ffc52a2405dfb5948e5&language='+year;
+    
+
+    if (keyword != '') {
+        $.ajax({url:url_first, method:'GET', async:true}).done(loadResponse);}
+
+    else if (year != '') {
+        $.ajax({url:url_second, method:'GET', async:true}).done(loadResponse);}
+    
+    else {alert('Please type something!');}
+
 };
 
 
